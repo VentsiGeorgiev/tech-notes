8495d2ec8c2e0b4534da87dcf3929eba97698eba
VentsiGeorgiev <ventsislavgeorgiev1@gmail.com>
2022-10-29 16:51:16 +0200

    Add login form;

--- client/src/component/pages/Login/Login.js
+++ client/src/component/pages/Login/Login.js
@@ -1,6 +1,42 @@
+import { Header } from '../../shared';
+import styles from './Login.module.scss';
+
 function Login() {
     return (
-        <div>Login</div>
+        <>
+            <Header />
+            <section className='container center'>
+                <form className='form'>
+                    <h2 className='form__title'>Login</h2>
+                    <div className='form__input'>
+                        <label
+                            htmlFor='username'
+                            className='form__input__label'
+                        >
+                            Username</label>
+                        <input
+                            id='username'
+                            type='text'
+                            name='username'
+                            className='form__input__field'
+                        />
+                    </div>
+                    <div className='form__input'>
+                        <label
+                            htmlFor='password'
+                            className='form__input__label'
+                        >Password</label>
+                        <input
+                            id='password'
+                            type='password'
+                            name='password'
+                            className='form__input__field'
+                        />
+                    </div>
+                    <button className='btn btn-login'>Submit</button>
+                </form>
+            </section>
+        </>
     );
 }
 
