42da280380583823f359a95c69dc533b43c10190
VentsiGeorgiev <ventsislavgeorgiev1@gmail.com>
2022-10-29 18:32:20 +0200

    Login, Logout - Server;

--- server/server.js
+++ server/server.js
@@ -1,13 +1,16 @@
 const express = require('express');
 const connectDB = require('./config/db');
 const dotenv = require('dotenv').config();
+const cookieParser = require('cookie-parser');
 const PORT = process.env.PORT || 8000;
 
 connectDB();
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
 
+app.use('/api/auth', require('./routes/auth'));
 app.use('/api/users', require('./routes/user'));
 app.use('/api/notes', require('./routes/note'));
 
