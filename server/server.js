4e81b4955cc9c018db014b16415e23b9d2c4441e
VentsiGeorgiev <ventsislavgeorgiev1@gmail.com>
2022-10-31 16:23:21 +0100

    Prepare for deploy;

--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@ const connectDB = require('./config/db');
 const dotenv = require('dotenv').config();
 const cookieParser = require('cookie-parser');
 const cors = require('./middleware/cors');
+const path = require('path');
 const PORT = process.env.PORT || 8000;
 
 connectDB();
@@ -16,8 +17,19 @@ app.use('/api/auth', require('./routes/auth'));
 app.use('/api/users', require('./routes/user'));
 app.use('/api/notes', require('./routes/note'));
 
-app.get('/', (req, res) => {
-    res.json({ message: 'techNotes' });
-});
+
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, '../client/build')));
+
+    app.get('*', (_, res) => {
+        res.sendFile(path.join(__dirname, '../client/build/index.html'));
+    });
+
+} else {
+    app.get('/', (req, res) => {
+        res.json({ message: 'techNotes' });
+    });
+}
+
 
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
