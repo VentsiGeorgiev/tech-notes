6f3530916605e47388fd6b06e2b8647e3d201347
VentsiGeorgiev <ventsislavgeorgiev1@gmail.com>
2022-10-31 12:56:56 +0100

    Styled dashboard component

--- client/src/component/pages/Dashboard/Dashboard.js
+++ client/src/component/pages/Dashboard/Dashboard.js
@@ -1,5 +1,6 @@
 import useAuth from '../../../hooks/useAuth';
 import { Link } from 'react-router-dom';
+import styles from './Dashboard.module.scss';
 
 function Dashboard() {
 
@@ -7,11 +8,30 @@ function Dashboard() {
 
     return (
         <main className='container'>
-            <p>welcome {username}!</p>
-
-            <p><Link to="/dashboard/notes">View techNotes</Link></p>
-
-            {(isManager || isAdmin) && <p><Link to="/dashboard/users">View User Settings</Link></p>}
+            <section className={styles.dashboard}>
+                <h2 className={styles.dashboard__message}>Welcome {username}!</h2>
+                <div className={styles.dashboard__notes}>
+                    <h3 className={styles.dashboard__notes__title}>Notes</h3>
+                    <ul>
+                        <li>
+                            <Link className='btn' to="/dashboard/notes">View techNotes</Link>
+                        </li>
+                    </ul>
+                </div>
+                <div className={styles.dashboard__users}>
+                    <h3 className={styles.dashboard__users__title}>User</h3>
+                    {(isManager || isAdmin) &&
+                        <ul>
+                            <li>
+                                <Link className='btn' to="/dashboard/users/add">Add New User</Link>
+                            </li>
+                            <li>
+                                <Link className='btn' to="/dashboard/users">View User Settings</Link>
+                            </li>
+                        </ul>
+                    }
+                </div>
+            </section>
 
         </main>
     );
